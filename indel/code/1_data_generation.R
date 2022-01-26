62d9ebf8494bde8357038d140af9fc999ad417d1
jnh01 <44010028+jnh01@users.noreply.github.com>
2022-01-26 19:18:40 +0800

    Updated mutograph_plot.Rmd and specified required version for PCAWG7 package

--- indel/code/1_data_generation.R
+++ indel/code/1_data_generation.R
@@ -9,6 +9,12 @@ if (basename(getwd()) != "Liu_et_al_Sup_Files") {
 if (!requireNamespace("remotes", quietly = TRUE)) {
   install.packages("remotes")
 }
+if (!requireNamespace("cosmicsig", quietly = TRUE)) {
+  remotes::install_github(repo = "Rozen-Lab/cosmicsig", ref = "v1.0.7-branch")
+}
+if (!requireNamespace("PCAWG7", quietly = TRUE)) {
+  remotes::install_github(repo = "steverozen/PCAWG7", ref = "v0.1.3-branch")
+}
 if (!requireNamespace("SynSigGen", quietly = TRUE) ||
   packageVersion("SynSigGen") < "1.1.1") {
   remotes::install_github(
@@ -16,21 +22,15 @@ if (!requireNamespace("SynSigGen", quietly = TRUE) ||
     ref = "1.1.1-branch"
   )
 }
-if (!requireNamespace("PCAWG7", quietly = TRUE)) {
-  remotes::install_github(repo = "steverozen/PCAWG7", ref = "master")
-}
-if (!requireNamespace("cosmicsig", quietly = TRUE)) {
-  remotes::install_github(repo = "Rozen-Lab/cosmicsig", ref = "v1.0.7-branch")
-}
 
 # Restart R after installing the new packages
 .rs.restartR()
 
 source("./common_code/data_gen_utils.R")
 
-library(SynSigGen)
-library(PCAWG7)
 library(cosmicsig)
+library(PCAWG7)
+library(SynSigGen)
 
 ##################################################################
 ##                      Data preprocessing                      ##
