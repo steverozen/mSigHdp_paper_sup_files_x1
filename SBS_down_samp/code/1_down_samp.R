f1df1b602170115b2749ffe79a0d95d57ae948b8
Wu, Yang <wuyangff95@protonmail.com>
2022-06-08 17:20:49 +0800

    Added scripts to generate 3 data sets, down-sampled from SBS synthetic dataset.
    Also added the spectra and exposure files of these data sets.

---
+++ SBS_down_samp/code/1_down_samp.R
@@ -0,0 +1,105 @@
+# Please run this script from the top directory
+if (basename(getwd()) != "Liu_et_al_Sup_Files") {
+  stop("Please run from top level directory, Liu_et_al_Sup_Files")
+}
+
+# 1. Install and load dependencies --------------------------------------------
+if (!requireNamespace("magrittr", quietly = TRUE)) {
+  install.packages("magrittr")
+}
+if (!requireNamespace("ICAMS", quietly = TRUE)) {
+  remotes::install_github("steverozen/ICAMS", ref = "v3.0.5-branch")
+}
+require(magrittr)
+require(ICAMS)
+# Source script to downsample exposure
+source("common_code/down_sample.R")
+
+
+# 2. Copy and shorten the names of folders and files under input/raw ----------
+old_SBS_home <- "SBS/input/Realistic"
+SBS_home <- "SBS_down_samp/input"
+dir.create(SBS_home, showWarnings = FALSE, recursive = TRUE)
+# Threshold values
+thres_vals <- c(1000, 5000, 10000)
+dataset_names <- c("1k", "5k", "10k")
+names(dataset_names) <- as.character(thres_vals)
+for (thres_val in thres_vals) {
+  dataset_path <- 
+    file.path(SBS_home, 
+              dataset_names[as.character(thres_val)])
+  if (!dir.exists(dataset_path)) 
+    dir.create(dataset_path, recursive = T)
+  
+  # Down-sample original exposure ---------------------------------------------
+  spec <- ICAMS::ReadCatalog(
+    file = paste0(old_SBS_home, "/ground.truth.syn.catalog.csv"),
+    ref.genome = "GRCh37",
+    region = "genome",
+    catalog.type = "counts")
+  exp <- mSigAct::ReadExposure(
+    paste0(old_SBS_home, "/ground.truth.syn.exposures.csv"))
+  exp_sum <- colSums(exp)
+  down_exp_sum <- sapply(exp_sum, down_sample_func, thres = thres_val)
+  
+  down_factor <- down_exp_sum / exp_sum
+  
+  # Export down-sampled exposure ----------------------------------------------
+  down_exp <- (t(exp) * down_factor) %>% t() %>% as.data.frame()
+  foo <- sapply(down_exp, round)
+  foo <- foo %>% as.data.frame()
+  # Check
+  if (FALSE) {
+    # Expects to be TRUE
+    all.equal(dimnames(exp), dimnames(down_exp))
+    # The difference between each element in two dfs is less than 1
+    foo - down_exp
+  }
+  dimnames(foo) <- dimnames(exp)
+  down_exp <- foo
+  rm(foo)
+  mSigAct::WriteExposure(
+    exposure = down_exp,
+    file = paste0(dataset_path, "/ground.truth.syn.exposures.csv"))
+  
+  # Export down-sampled catalog -----------------------------------------------
+  down_spec <-(t(spec) * down_factor) %>% t() %>% as.data.frame()
+  foo <- sapply(down_spec, round)
+  foo <- foo %>% as.data.frame()
+  # Check
+  if (FALSE) {
+    # Expects to be TRUE
+    all.equal(dimnames(spec), dimnames(down_spec))
+    # The difference between each element in two dfs is less than 1
+    foo - down_spec
+  }
+  dimnames(foo) <- dimnames(spec)
+  down_spec <- ICAMS::as.catalog(foo, ref.genome = "GRCh37",
+                                 region = "genome",
+                                 catalog.type = "counts")
+  rm(foo)
+  ICAMS::WriteCatalog(
+    down_spec,
+    file = paste0(dataset_path, "/ground.truth.syn.catalog.csv"))
+  # Export ICAMS-formatted SBS spectra to SigPro tsv format 
+  ICAMS:::ConvertCatalogToSigProfilerFormat(
+    down_spec,
+    file = paste0(dataset_path, "/ground.truth.syn.catalog.tsv"),
+    sep = "\t")
+  
+  # Copy ground-truth signature file ------------------------------------------
+  old_sig_path <- paste0(old_SBS_home, "/ground.truth.syn.sigs.csv")
+  file.copy(from = old_sig_path,
+            to = dataset_path,
+            copy.date = TRUE)
+  sigs <- ICAMS::ReadCatalog(
+    old_sig_path,
+    ref.genome = "GRCh37",
+    region = "genome",
+    catalog.type = "counts.signature")
+  # Export ICAMS-formatted SBS signatures to SigPro tsv format 
+  ICAMS:::ConvertCatalogToSigProfilerFormat(
+    sigs,
+    file = paste0(dataset_path, "/ground.truth.syn.sigs.tsv"),
+    sep = "\t")
+}
