d048317154b743e62557be2a9c7be7155bcc1d02
Wu, Yang <wuyangff95@protonmail.com>
2022-06-13 19:38:41 +0800

    1. Added code to down-sample indel spectra. 2. Added files for down-sampled spectra, their ground-truth signatures and exposures. 3. Now "3k" and "20k" datasets are also generated by SBS spectra down-sampling code.

--- indel_down_samp/code/1_down_samp.R
+++ indel_down_samp/code/1_down_samp.R
@@ -17,28 +17,28 @@ source("common_code/down_sample.R")
 
 
 # 2. Copy and shorten the names of folders and files under input/raw ----------
-old_SBS_home <- "SBS/input/Realistic"
-SBS_home <- "SBS_down_samp/input"
-dir.create(SBS_home, showWarnings = FALSE, recursive = TRUE)
+old_indel_home <- "indel/input/Realistic"
+indel_home <- "indel_down_samp/input"
+dir.create(indel_home, showWarnings = FALSE, recursive = TRUE)
 # Threshold values
-thres_vals <- c(1000, 3000, 5000, 10000, 20000)
-dataset_names <- c("1k", "3k", "5k", "10k", "20k")
+thres_vals <- c(500, 1000, 3000, 5000, 10000)
+dataset_names <- c("500", "1k", "3k", "5k", "10k")
 names(dataset_names) <- as.character(thres_vals)
 for (thres_val in thres_vals) {
   dataset_path <- 
-    file.path(SBS_home, 
+    file.path(indel_home, 
               dataset_names[as.character(thres_val)])
   if (!dir.exists(dataset_path)) 
     dir.create(dataset_path, recursive = T)
   
   # Down-sample original exposure ---------------------------------------------
   spec <- ICAMS::ReadCatalog(
-    file = paste0(old_SBS_home, "/ground.truth.syn.catalog.csv"),
+    file = paste0(old_indel_home, "/ground.truth.syn.catalog.csv"),
     ref.genome = "GRCh37",
     region = "genome",
     catalog.type = "counts")
   exp <- mSigAct::ReadExposure(
-    paste0(old_SBS_home, "/ground.truth.syn.exposures.csv"))
+    paste0(old_indel_home, "/ground.truth.syn.exposures.csv"))
   exp_sum <- colSums(exp)
   # Print the number of samples with mutations smaller than thres_val
   which(exp_sum <= thres_val) %>% length() %>% print()
@@ -83,14 +83,14 @@ for (thres_val in thres_vals) {
   ICAMS::WriteCatalog(
     down_spec,
     file = paste0(dataset_path, "/ground.truth.syn.catalog.csv"))
-  # Export ICAMS-formatted SBS spectra to SigPro tsv format 
+  # Export ICAMS-formatted indel spectra to SigPro tsv format 
   ICAMS:::ConvertCatalogToSigProfilerFormat(
     down_spec,
     file = paste0(dataset_path, "/ground.truth.syn.catalog.tsv"),
     sep = "\t")
   
   # Copy ground-truth signature file ------------------------------------------
-  old_sig_path <- paste0(old_SBS_home, "/ground.truth.syn.sigs.csv")
+  old_sig_path <- paste0(old_indel_home, "/ground.truth.syn.sigs.csv")
   file.copy(from = old_sig_path,
             to = dataset_path,
             copy.date = TRUE)
@@ -99,7 +99,7 @@ for (thres_val in thres_vals) {
     ref.genome = "GRCh37",
     region = "genome",
     catalog.type = "counts.signature")
-  # Export ICAMS-formatted SBS signatures to SigPro tsv format 
+  # Export ICAMS-formatted indel signatures to SigPro tsv format 
   ICAMS:::ConvertCatalogToSigProfilerFormat(
     sigs,
     file = paste0(dataset_path, "/ground.truth.syn.sigs.tsv"),
