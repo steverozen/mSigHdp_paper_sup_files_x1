6a3df782c359bf2485334c86f61627b5c5170aba
Steve Rozen <steverozen@gmail.com>
2022-06-22 17:31:56 +0800

    code / scripts to qsub NR hdp on SBS

---
+++ SBS/code/SBS.4f_SBS_NR_hdp_gamma_beta_1.528401.sh
@@ -0,0 +1,9 @@
+#PBS -q super
+#PBS -l nodes=1:ppn=20
+#PBS -N 4f_SBS_NR_hdp_gamma_beta_1.528401
+#PBS -o SBS/code/SBS.4f_SBS_NR_hdp_gamma_beta_1.528401.sh.out
+#PBS -e SBS/code/SBS.4f_SBS_NR_hdp_gamma_beta_1.528401.sh.err
+#PBS -S /bin/bash
+cd $PBS_O_WORKDIR
+mkdir -p SBS/raw_results/4f_SBS_NR_hdp_gamma_beta_1
+nice Rscript SBS/code/4f_SBS_NR_hdp_gamma_beta_1.R SBS/raw_results/4f_SBS_NR_hdp_gamma_beta_1 528401 >& SBS/raw_results/4f_SBS_NR_hdp_gamma_beta_1/528401.log
