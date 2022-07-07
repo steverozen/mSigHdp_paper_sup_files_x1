c4bf8674e73fcbeede3230aa4643b31d8b614a7a
WuyangFF95 <wuyangff95@protonmail.com>
2022-07-07 16:41:31 +0800

    Continue fixing 3a_run_mSigHdp.R and wrapper shell script

--- indel_down_samp/code/3a_145879.sh
+++ indel_down_samp/code/3a_145879.sh
@@ -1,12 +1,23 @@
-#PBS -q long
-#PBS -l nodes=1:ppn=20
+#!/bin/bash
+#PBS -q super
+#PBS -l walltime=360:00:00
+#PBS -l select=1:ncpus=20:mem=200gb
 #PBS -N 3a_145879
-#PBS -o 3a_145879_out.txt
-#PBS -e 3a_145879_err.txt
-#PBS -S /bin/bash
+#PBS -o indel_down_samp/raw_results/3a_145879_out.txt
+#PBS -e indel_down_samp/raw_results/3a_145879_err.txt
+HOME_LOC=/data/rozen/home/e0240162
+PROJ_LOC=${HOME_LOC}/practice/6_Mo_mSigHdp/mSigHdp_paper_sup_files_x1
+ANACOND_BIN=${HOME_LOC}/opt/anaconda3/bin/conda
+ANACOND_RSCRIPT=${HOME_LOC}/opt/anaconda3/envs/R-4.1.3/bin/Rscript
 
-cd $PBS_O_WORKDIR/
+SEED=145879
 
+cd $PROJ_LOC
 echo "Start running the wrapper script ......"
-mkdir -p indel_down_samp/raw_results/mSigHdp.results/non_hyper
-nice /data/rozen/home/e0240162/opt/anaconda3/envs/R-4.1.3/bin/Rscript indel_down_samp/code/3a_run_mSigHdp.R 145879 &>> indel_down_samp/raw_results/mSigHdp.results/3a_145879.out
+mkdir -p ${PROJ_LOC}/indel_down_samp/raw_results/mSigHdp.results/non_hyper/
+$ANACOND_BIN activate R-4.1.3
+$ANACON_RSCRIPT ${PROJ_LOC}/indel_down_samp/code/3a_run_mSigHdp.R $SEED &>> ${PROJ_LOC}/indel_down_samp/raw_results/mSigHdp.results/3a_${SEED}.out
+$ANACOND_BIN deactivate
+$ANACOND_BIN deactivate
+exit 0
+
