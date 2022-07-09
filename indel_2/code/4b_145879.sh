5441cf2a724594c29e70397a0e40cb1a6c158626
Wu, Yang <wuyangff95@protonmail.com>
2022-07-09 17:00:37 +0800

    Fixed bugs in PBS shell script:
    1. Changed "mem:200gb" to "mem=200gb"
    2. PBS -o and -e requires ABSOLUTE directory
    3. source $CONDA_BIN_DIR/activate before running conda activate
    
    Also added shell script to run SignatureAnalyzer on indel_2.

--- indel_2/code/4b_145879.sh
+++ indel_2/code/4b_145879.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #PBS -q super
 #PBS -l walltime=360:00:00
-#PBS -l select=1:ncpus=20:mem=200gb
-#PBS -N 3a_145879
-#PBS -o /data/rozen/home/e0240162/practice/6_Mo_mSigHdp/mSigHdp_paper_sup_files_x1/indel_down_samp/raw_results/3a_145879_out.txt
-#PBS -e /data/rozen/home/e0240162/practice/6_Mo_mSigHdp/mSigHdp_paper_sup_files_x1/indel_down_samp/raw_results/3a_145879_err.txt
+#PBS -l select=1:ncpus=50:mem=200gb
+#PBS -N 4b_145879
+#PBS -o /data/rozen/home/e0240162/practice/6_Mo_mSigHdp/mSigHdp_paper_sup_files_x1/indel_2/raw_results/4b_145879_out.txt
+#PBS -e /data/rozen/home/e0240162/practice/6_Mo_mSigHdp/mSigHdp_paper_sup_files_x1/indel_2/raw_results/4b_145879_err.txt
 HOME_LOC=/data/rozen/home/e0240162
 PROJ_LOC=$HOME_LOC/practice/6_Mo_mSigHdp/mSigHdp_paper_sup_files_x1
 CONDA_BIN_DIR=$HOME_LOC/opt/anaconda3/bin
@@ -19,8 +19,8 @@ $CONDA_BIN activate R-4.1.3
 
 cd $PROJ_LOC
 echo "Start running the wrapper script ......"
-mkdir -p $PROJ_LOC/indel_down_samp/raw_results/mSigHdp.results/non_hyper/
-$CONDA_RSCRIPT $PROJ_LOC/indel_down_samp/code/3a_run_mSigHdp.R $SEED &>> $PROJ_LOC/indel_down_samp/raw_results/mSigHdp.results/3a_${SEED}.out
+mkdir -p $PROJ_LOC/indel_2/raw_results/SignatureAnalyzer.results/non_hyper/
+$CONDA_RSCRIPT $PROJ_LOC/indel_2/code/4b_run_SignatureAnalyzer.R $SEED &>> $PROJ_LOC/indel_down_samp/raw_results/SignatureAnalyzer.results/4b_${SEED}.out
 
 $CONDA_BIN deactivate #this is to exit the $ENV_NAME environment
 $CONDA_BIN deactivate #this is to exit the base anaconda environment
