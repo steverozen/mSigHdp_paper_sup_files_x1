f9388aaa540157b843d93b7f3faecc5ff76f75ac
Wu, Yang <wuyangff95@protonmail.com>
2022-07-11 11:19:24 +0800

    1. Fixed PBS arguments in indel/code/145879.sh so that it can run anaconda R via PBS
    2. Renamed PBS shell scripts
    
    indel/code/4e_<seed>.sh
    to
    indel/code/4e_<seed>_steve.sh
    
    This is because these codes could be successfully submit to PBS when running with R in signularity container by Steve, but failed when running with R in anaconda environment by Wu Yang.

--- indel/code/4e_145879.sh
+++ indel/code/4e_145879.sh
@@ -1,11 +1,24 @@
+#!/bin/bash
 #PBS -q super
-#PBS -l nodes=1:ppn=20
+#PBS -l walltime=360:00:00
+#PBS -l select=1:ncpus=20:mem=200gb
 #PBS -N 4e_145879
-#PBS -o 145879_out.txt
-#PBS -e 145879_err.txt
-#PBS -S /bin/bash
+#PBS -o /data/rozen/home/e0240162/practice/6_Mo_mSigHdp/mSigHdp_paper_sup_files_x1/indel/raw_results/NR_hdp_gb_50_145879_out.txt
+#PBS -e /data/rozen/home/e0240162/practice/6_Mo_mSigHdp/mSigHdp_paper_sup_files_x1/indel/raw_results/NR_hdp_gb_50_145879_err.txt
+HOME_LOC=/data/rozen/home/e0240162
+PROJ_LOC=$HOME_LOC/practice/6_Mo_mSigHdp/mSigHdp_paper_sup_files_x1
+CONDA_BIN_DIR=$HOME_LOC/opt/anaconda3/bin
+CONDA_BIN=$CONDA_BIN_DIR/conda
+CONDA_RSCRIPT=$HOME_LOC/opt/anaconda3/envs/R-4.1.3/bin/Rscript
 
-cd $PBS_O_WORKDIR/
+SEED=145879
 
+# Initiate conda on computation node
+$CONDA_BIN init bash
+
+cd $PROJ_LOC
+echo "Start running the wrapper script ......"
 mkdir -p indel/raw_results/NR_hdp_gamma_beta_50
-nice Rscript indel/code/4e_run_NR_hdp_gamma_beta_50.R 145879 >& indel/raw_results/NR_hdp_gamma_beta_50/145879.log
+nice Rscript indel/code/4e_run_NR_hdp_gamma_beta_50.R $SEED &>> indel/raw_results/NR_hdp_gamma_beta_50/${SEED}.log
+
+exit 0
