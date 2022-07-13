2915124fabdab67539b8636e0ba97ff9a2dfa447
Wu, Yang <wuyangff95@protonmail.com>
2022-07-13 18:52:21 +0800

    Added summary code for data sets "indel_2"

---
+++ indel_2/code/9_plotting.R
@@ -0,0 +1,14 @@
+# Please run this script from the top directory
+if (basename(getwd()) != "mSigHdp_paper_sup_files_x1") {
+  stop("Please run from top level directory, mSigHdp_paper_sup_files_x1")
+}
+
+home_for_data <- "./indel_2/input/"
+home_for_summary <- "./indel_2/summary/"
+plot.meta.info <- paste("indel",
+                        Sys.Date(), 
+                        "Realistic size = 10; gamma.beta = 50, burnin = 30k")
+tool_names <- c("mSigHdp", "SigProfilerExtractor",
+                "SignatureAnalyzer", "signeR")
+
+source("common_code/main_text_plotting.R")
