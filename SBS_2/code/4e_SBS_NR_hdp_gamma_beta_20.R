f0e3b5503bf8d7825ebe737e9c8b0ca0d1597117
Wu, Yang <wuyangff95@protonmail.com>
2022-08-15 13:04:49 +0800

    Add script to run Nicola Robert's hdp (with gamma.beta argument changed) on SBS_2/Realistic data set

---
+++ SBS_2/code/4e_SBS_NR_hdp_gamma_beta_20.R
@@ -0,0 +1,41 @@
+# How to run:
+# $ nice Rscript SBS_2/code/4e_SBS_run_NR_hdp_gamma_beta_20.R >>& SBS_2/raw_results/NR_hdp_gb_20.results/log &
+
+basedir <- "mSigHdp_paper_sup_files_x1" 
+if (basename(getwd()) != basedir) {
+  stop("Please run from top level directory, ", basedir)
+}
+
+message(Sys.time(), " running ", paste(commandArgs(), collapse = " "))
+
+args <- commandArgs(trailingOnly = TRUE)
+
+if (length(args) > 0) {
+  seeds_in_use <- args
+} else {
+  seeds_in_use <- c(145879, 200437, 310111, 528401, 1076753)
+}
+
+message(Sys.time(), " running on seed ", seeds_in_use)
+
+# Set global variables ---------------------------------------------------------
+GLOBAL.gamma.alpha <- 1  # This will be used inside mSigHdp::SetupAndPosterior;
+                         # alpha is also called the shape parameter
+GLOBAL.gamma.beta  <- 20 # This will be used inside mSigHdp::SetupAndPosterior;
+                         # beta is also called the rate parameter
+burnin.iterations  <- 5000 * 20
+CPU.cores          <- 20
+num.child.process  <- 20
+# Guessed number of raw clusters
+start_K            <- 64
+
+home_for_run       <- paste0("./SBS_2/raw_results/NR_hdp_gb_", 
+                             GLOBAL.gamma.beta, ".results/")
+home_for_data      <- "./SBS/input"
+
+# Names of data sets
+dataset_names <- c("Realistic")
+
+# Run mSigHdp -----------------------------------------------------------------
+# Install and load package versions to test Nicola Roberts's algorithms 
+source("common_code/generic_run_NR_hdp.R")
