be0990a4de046fe2b433070f6b0ac4c89f119298
Steven G. Rozen <steve@steverozen.net>
2022-08-17 21:28:27 -0400

    finished figures for which we already have data

--- common_code/steve_plots.R
+++ common_code/steve_plots.R
@@ -4,41 +4,54 @@ library(magrittr)
 library(dplyr)
 library(beeswarm)
 
+split_by_approach_and_pull <- function(vv, approach.to.use) {
+  xxs     <- split(vv, vv$Approach)
+  xxs2    <- xxs[approach.to.use]
+  comp <- lapply(xxs2, pull, "Composite")
+  tpr  <- lapply(xxs2, pull, "TPR")
+  ppv  <- lapply(xxs2, pull, "PPV")
+  sim  <- lapply(xxs2, pull, "aver_Sim_TP_only")
+  return(list(split = xxs2, comp = comp, tpr = tpr, ppv = ppv, sim =sim))
+}
+
+four_beeswarms <- function(ww, main, col, pch, filename) {
+  grDevices::cairo_pdf(
+    filename = filename,
+    height = 9)
+  par(mfrow = c(2,2), mar = c(9, 4, 4, 2) + 0.1)
+  
+  beeswarm(x = ww$comp, las = 2, ylab = "Composite measure", 
+           pwpch = pch, pwcol = col,
+           main = main)
+  beeswarm(x = ww$ppv, las = 2,  ylab = "PPV", 
+           pwpch = pch, pwcol = col)
+  beeswarm(x = ww$ppv, las = 2, ylab = "TPR", 
+           pwpch = pch, pwcol = col)
+  beeswarm(x = ww$sim, las = 2, ylab = "Cosine similarity", 
+           pwpch = pch, pwcol = col)
+  grDevices::dev.off()
+  
+}
+
 generic_beeswarm_fig <-
   function(tt, approach.to.use, sbs.or.indel, file.name.prefix) {
 
   set1 <- paste0(sbs.or.indel, "_set1")
   set2 <- paste0(sbs.or.indel, "_set2")
   t1 <- filter(tt, Noise_level == "Realistic")
-  t2 <-  filter(t1, Approach %in% approach.to.use)
-  xx <- filter(t2, Data_set %in% c(set1, set2))
+  xx <- filter(t1, Data_set %in% c(set1, set2))
 
-  xxs <- split(xx, xx$Approach)
-  xxs2 <- xxs[approach.to.use]
-  xx.comp     <- lapply(xxs2, pull, "Composite")
-  xx.tpr      <- lapply(xxs2, pull, "TPR")
-  xx.ppv      <- lapply(xxs2, pull, "PPV")
-  xx.sim      <- lapply(xxs2, pull, "aver_Sim_TP_only")
-  xx.data.set <- unlist(lapply(xxs2, pull, "Data_set"))
+  ww <- split_by_approach_and_pull(xx, approach.to.use)
+  xx.data.set <- unlist(lapply(ww$split, pull, "Data_set"))
 
   col <- ifelse(xx.data.set == set1, "red", "blue")
   pch <- ifelse(xx.data.set == set1, 16, 17)
   
-  grDevices::cairo_pdf(
-    filename = paste0(file.name.prefix, sbs.or.indel, ".pdf"),
-    height = 9)
-  par(mfrow = c(2,2), mar = c(9, 4, 4, 2) + 0.1)
-  
-  beeswarm(x = xx.comp, las = 2, ylab = "Composite measure", 
-           pwpch = pch, pwcol = col,
-           main = paste0(sbs.or.indel, "; red = set1, blue = set2"))
-  beeswarm(x = xx.ppv, las = 2,  ylab = "PPV", 
-           pwpch = pch, pwcol = col)
-  beeswarm(x = xx.tpr, las = 2, ylab = "TPR", 
-           pwpch = pch, pwcol = col)
-  beeswarm(x = xx.sim, las = 2, ylab = "Cosine similarity", 
-           pwpch = pch, pwcol = col)
-  grDevices::dev.off()
+  four_beeswarms(ww, 
+                 main = paste0(sbs.or.indel, "; red = set1, blue = set2"),
+                 col,
+                 pch,
+                 filename = paste0(file.name.prefix, sbs.or.indel, ".pdf"))
 }
 
 main_text_SBS_fig <- function(tt) {
@@ -63,8 +76,8 @@ main_text_indel_fig <- function(tt) {
       "SigProfilerExtractor",
       "NR_hdp_gb_50", 
       "NR_hdp_gb_1",
-      "signeR",
-      "SignatureAnalyzer"     )
+      "SignatureAnalyzer",
+      "signeR")
   
   generic_beeswarm_fig(tt, approach.to.use, "indel",  "draft_main_text_fig_")
 }
@@ -102,3 +115,41 @@ main_text_indel_fig(tt)
 main_text_SBS_fig(tt)
 downsample_indel_fig(tt)
 downsample_SBS_fig(tt)
+
+noise_level_fig <- function(tt, indel.or.sbs, approach) {
+
+  # filter by approach and by indel_set1
+  # split by approach, color by noise level
+  data.set = paste0(indel.or.sbs, "_set1")
+  tt1 <- dplyr::filter(tt, Data_set == data.set)
+  ww <- split_by_approach_and_pull(tt1, approach)
+  xx.noise.level <- unlist(lapply(ww$split, pull, "Noise_level"))
+  
+  # Color by noise
+  col <- ifelse(xx.noise.level == "Realistic", "red",
+                ifelse(xx.noise.level == "Moderate", "violet", "blue"))
+  
+  pch <- ifelse(xx.noise.level == "Realistic", 16,
+                ifelse(xx.noise.level == "Moderate", 17, 18))
+  
+  four_beeswarms(
+    ww,
+    main = paste0(indel.or.sbs, "\nred = Realistic, violet = Moderate, blue = Noiseless"),
+    col,
+    pch,
+    filename = paste0(indel.or.sbs, "_noise.pdf"))
+
+}
+
+noise_level_fig(tt, "indel",approach = c("mSigHdp",
+                                         "SigProfilerExtractor",
+                                         "SignatureAnalyzer",
+                                         "signeR")) # Order of SA and signeR are reversed between indel and SBS
+
+
+noise_level_fig(tt, "SBS",approach = c("mSigHdp_ds_3k",
+                                       "mSigHdp",
+                                       "SigProfilerExtractor",
+                                       "signeR",
+                                       "SignatureAnalyzer"
+))
