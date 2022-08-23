6c125e274cf41fc791d6fe913cb1ae3841b6af0c
Steven G. Rozen <steve@steverozen.net>
2022-08-22 20:55:03 -0400

    re-ran; regenerated all PDFs

--- common_code/summarize_level1_dirs.R
+++ common_code/summarize_level1_dirs.R
@@ -25,7 +25,7 @@ require(SynSigEval)
 require(readr)
 }
 
-library(data.table)
+library(tibble)
 
 extract_from_one_seeds_summary <- function(summary.directory.path) {
   gt <- ICAMS::ReadCatalog(file.path(summary.directory.path, "ground.truth.sigs.csv"))
@@ -42,17 +42,19 @@ summarize_level1_dirs <- function(a.folder) {
   dataset.name.to.use <- sub("_down_samp", "", a.folder)
   message("using dataset name ", dataset.name.to.use)
   
-  out <- data.table(Data_set         = "",
-                    Noise_level      = "",
-                    Approach         = "",
-                    Run              = "",
-                    PPV              = -1,
-                    TPR              = -1,
-                    aver_Sim_TP_only = -1,
-                    Composite        = -1,
-                    N_Sigs           = -1,
-                    FN               = -1,
-                    FP               = -1)
+  out <- tibble(Data_set         = "",
+                Noise_level      = "",
+                Approach         = "",
+                Run              = "",
+                PPV              = -1,
+                TPR              = -1,
+                aver_Sim_TP_only = -1,
+                Composite        = -1,
+                N_Sigs           = -1,
+                FN               = -1,
+                FP               = -1,
+                FP.sigs          = character(0),
+                FN.sigs          = character(0))
   
   start.here <- file.path(a.folder, "raw_results")
   
@@ -107,18 +109,20 @@ summarize_level1_dirs <- function(a.folder) {
         } # else SignatureAnalyzer
         
         tff <- extract_from_one_seeds_summary(file.path(seedInUse, "summary"))
-        row <- data.table(Data_set         = dataset.name.to.use,
-                          Noise_level      = noise.level,
-                          Approach         = toolName,
-                          Run              = basename(seedInUse),
-                          PPV              = tff$PPV,
-                          TPR              = tff$TPR,
-                          aver_Sim_TP_only = tff$avg.cos.sim,
-                          Composite        = tff$PPV + tff$TPR + tff$avg.cos.sim,
-                          N_Sigs           = tff$TP + tff$FP,
-                          FN               = tff$FN,
-                          FP               = tff$FP)
-
+        row <- tibble(Data_set         = dataset.name.to.use,
+                      Noise_level      = noise.level,
+                      Approach         = toolName,
+                      Run              = basename(seedInUse),
+                      PPV              = tff$PPV,
+                      TPR              = tff$TPR,
+                      aver_Sim_TP_only = tff$avg.cos.sim,
+                      Composite        = tff$PPV + tff$TPR + tff$avg.cos.sim,
+                      N_Sigs           = tff$TP + tff$FP,
+                      FN               = tff$FN,
+                      FP               = tff$FP,
+                      FP.sigs          = tff$unmatched.ex.sigs,
+                      FN.sigs          = tff$unmatched.ref.sigs)
+        
         out <- rbind(out, row)
         
       } # for(seedInUse)
@@ -127,7 +131,8 @@ summarize_level1_dirs <- function(a.folder) {
   } # for (analysis.name in tools)
   
   out <- out[-1, ]
-  data.table::fwrite(data.table::as.data.table(out), file.path(a.folder, "all_sub_results.csv"))
+  readr::write_csv(data.table::as.data.table(out), file.path(a.folder, "new_all_sub_results.csv"))
+  save(out, file=file.path(a.folder, "level1_results.Rdata"))
   invisible(out)
 } # function summarize_something
 
@@ -144,11 +149,12 @@ level1.dirs <- c("indel_set1",
 
 all.out.list <- lapply(level1.dirs, summarize_level1_dirs)
 
-all.out <- do.call(rbind, all.out.list)
+all.results <- do.call(rbind, all.out.list)
 
-data.table::fwrite(all.out, "all_results_by_seed.csv")
+readr::write_csv(all.out, "new_all_results_by_seed.csv")
+save(all.results, file = "all_results_by_seed.Rdata")
 
-tt <- tibble::as_tibble(all.out)
+# tt <- tibble::as_tibble(all.out)
 
 tt.indel <- dplyr::filter(tt, Data_set %in% c("indel_set1", "indel_set2"))
 tt.SBS   <- dplyr::filter(tt, Data_set %in% c("SBS_set1",   "SBS_set2"))
