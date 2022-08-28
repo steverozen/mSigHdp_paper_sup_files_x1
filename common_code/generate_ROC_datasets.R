843b028123db27b503b844f7b7562000fd545b49
Steven G. Rozen <steve@steverozen.net>
2022-08-27 21:48:42 -0400

    more work on "spiked in rare signature" input synthetic data sets

--- common_code/generate_ROC_datasets.R
+++ common_code/generate_ROC_datasets.R
@@ -3,63 +3,100 @@ if (basename(getwd()) != basedir) {
   stop("Please run from top level directory, ", basedir)
 }
 
-spike.in.sig <- "SBS35"
+library(ICAMS)
+library(mSigTools)
+library(fitdistrplus)
+library(PCAWG7)
 
-which.set <- "SBS_set1/input/Realistic"
-exposures <- mSigTools::read_exposure(
-  file.path(which.set, "ground.truth.syn.exposures.csv"))
-signatures <- ICAMS::ReadCatalog(
-  file.path(which.set, "ground.truth.syn.sigs.csv"))
-spectra <- ICAMS::ReadCatalog(
-  file.path(which.set, "ground.truth.syn.catalog.csv"))
-
-with.sig <- which(exposures[spike.in.sig, ] > 0)
-
-with.sig.exp <- (exposures[spike.in.sig, with.sig])
-
-mean.exp <- mean(with.sig.exp)
-sd.exp   <- sd(with.sig.exp)
-spectra.no.target.sig <- spectra[ , - with.sig]
-
-# create 100 partial spectra of spike.in.sig according to mean.exp and sd.exp 
-
-spike.sig.profile <- signatures[ , spike.in.sig, drop = FALSE]
-
-set.seed(1066)
-sig.counts <- rnorm(n = 108, mean = mean.exp, sd = sd.exp)
-low.counts <- which(sig.counts < 100)
-
-sig.counts <- sig.counts[-low.counts]
-
-# Partial spectru due to the spiked in signature
-spike.partial.spectra <- round(spike.sig.profile %*% sig.counts)
-
-# Nanhai, how do you recommend we add noise to the spike.partial.spectra? 
- 
-
-one_spike_set <- function(num.spiked, out.file.name) {
+generate_spiked_data_sets <- function(
+    spike.in.sig = "SBS35",
+    which.set    = "SBS_set1/input/Realistic",
+    exposures    = mSigTools::read_exposure(
+      file.path(which.set, "ground.truth.syn.exposures.csv")),
+    rand.seed    = 1066)
+  {
   
-  indices.to.spike  <- # num.spiked column indices of spectra.no.target.sig 
-    sample(x = ncol(spectra.no.target.sig), size = num.spiked, replace = FALSE)
+  signatures = ICAMS::ReadCatalog(
+    file.path(which.set, "ground.truth.syn.sigs.csv"))
   
-  new.spectra <- spectra.no.target.sig
+  spectra <- ICAMS::ReadCatalog(
+    file.path(which.set, "ground.truth.syn.catalog.csv"))
   
-  partial.spectra.indices.to.use <- # num.spiked column indices of spike.partial.spectra
-    sample(x = ncol(spike.partial.spectra), size = num.spiked, replace = FALSE)
+  spectra.no.target.sig <- spectra[ , -(which(exposures[spike.in.sig, ] > 0))]
   
-  new.spectra[  , indices.to.spike] <- 
-    new.spectra[ , indices.to.spike] + 
-    spike.partial.spectra[, partial.spectra.indices.to.use]
+  p7.exp <- PCAWG7::exposure$PCAWG$SBS96[spike.in.sig,  , drop = FALSE]
+  ot.exp <- PCAWG7::exposure$other.genome$SBS96[spike.in.sig,  , drop = FALSE]
+  with.sig.exp <- c(p7.exp[p7.exp > 0], ot.exp[ot.exp > 9])
   
-  ICAMS::WriteCatalog(new.spectra, out.file.name)
+  # Get maximum likelihood estimates of parameters from negative binomial
+  # distribution
+  fit <- fitdistrplus::mledist(with.sig.exp, distr = "nbinom")
   
-  return(new.spectra)
+  mu.exp <- fit$estimate[2]
+  size.exp <- fit$estimate[1]
+  
+  spike.sig.profile <- signatures[ , spike.in.sig, drop = FALSE]
+  
+  set.seed(rand.seed)
+  sig.counts <- stats::rnbinom(n = 120, size = size.exp, mu = mu.exp)
+  low.counts <- which(sig.counts < 100)
+  if (length(low.counts) > 0) {
+    sig.counts <- sig.counts[-low.counts]
+  }
+  stopifnot(length(sig.counts) >= 100)
+  sig.counts <- sig.counts[1:100]
+  
+  # Partial spectra due to the spiked in signature
+  spike.partial.spectra <- round(spike.sig.profile %*% sig.counts)
+  
+  # Use the same dispersion parameter for adding noise to previous SBS data
+  n.binom.size <- 30 
+  
+  noised.vec <-
+    stats::rnbinom(n = length(spike.partial.spectra), 
+                   size = n.binom.size, 
+                   mu = spike.partial.spectra)
+  
+  # Turn the vector back into a matrix
+  noisy.spike.partial.spectra <- 
+    matrix(noised.vec, nrow = nrow(spike.partial.spectra))
+  rownames(noisy.spike.partial.spectra) <- rownames(spike.partial.spectra)
+  
+  one_spike_set <- function(num.spiked) {
+    
+    indices.to.spike  <- # num.spiked column indices of spectra.no.target.sig 
+      sample(x = ncol(spectra.no.target.sig),
+             size = num.spiked, 
+             replace = FALSE)
+    
+    new.spectra <- spectra.no.target.sig
+    
+    partial.spectra.indices.to.use <- # num.spiked column indices of spike.partial.spectra
+      sample(x = ncol(noisy.spike.partial.spectra),
+             size = num.spiked, 
+             replace = FALSE)
+    
+    new.spectra[  , indices.to.spike] <- 
+      new.spectra[ , indices.to.spike] + 
+      noisy.spike.partial.spectra[, partial.spectra.indices.to.use]
+    
+    
+    out.dir.name <- paste0("ROC_SBS_", num.spiked, "_", rand.seed)
+    new.dir <- file.path(out.dir.name, "input", "Realistic")
+    if (!dir.exists(new.dir)) {
+      dir.create(new.dir, recursive = TRUE)
+    }
+    
+    ICAMS::WriteCatalog(new.spectra,
+                        file.path(new.dir, "ground.truth.syn.catalog.csv"))
+    # Wu Yang, please add code to create sigpro input
+    ICAMS::WriteCatalog(signatures, "ground.truth.syn.sigs.csv")
+    
+    return(new.spectra)
+  }
+  
+  spiked.counts <- c(100, 50, 30, 20, 10, 5)
+  invisible(lapply(spiked.counts, one_spike_set))
 }
 
-s100 <- one_spike_set(100, "ROC_SBS_100.csv")
-s50  <- one_spike_set(50, "ROC_SBS_50.csv")
-s30  <- one_spike_set(30, "ROC_SBS_30.csv")
-s20  <- one_spike_set(20, "ROC_SBS_20.csv")
-s10  <- one_spike_set(10, "ROC_SBS_10.csv")
-s5   <- one_spike_set(5,  "ROC_SBS_5.csv")
-
+generate_spiked_data_sets()
