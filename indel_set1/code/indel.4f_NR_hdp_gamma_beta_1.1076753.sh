35d8e3d7b9250be13a8b1e8f2234478d0adb5e4f
Steven G. Rozen <steve@steverozen.net>
2022-09-03 16:22:12 -0400

    rename / move a few files

--- indel_set1/code/indel.4f_NR_hdp_gamma_beta_1.1076753.sh
+++ indel_set1/code/indel.4f_NR_hdp_gamma_beta_1.1076753.sh
@@ -1,9 +1,9 @@
 #PBS -q super
 #PBS -l nodes=1:ppn=20
 #PBS -N 4f_NR_hdp_gamma_beta_1.1076753
-#PBS -o indel/code/indel.4f_NR_hdp_gamma_beta_1.1076753.sh.out
-#PBS -e indel/code/indel.4f_NR_hdp_gamma_beta_1.1076753.sh.err
+#PBS -o indel_set1/code/indel.4f_NR_hdp_gamma_beta_1.1076753.sh.out
+#PBS -e indel_set1/code/indel.4f_NR_hdp_gamma_beta_1.1076753.sh.err
 #PBS -S /bin/bash
 cd $PBS_O_WORKDIR
-mkdir -p indel/raw_results/4f_NR_hdp_gamma_beta_1
-nice Rscript indel/code/4f_NR_hdp_gamma_beta_1.R indel/raw_results/4f_NR_hdp_gamma_beta_1 1076753 >& indel/raw_results/4f_NR_hdp_gamma_beta_1/1076753.log
+mkdir -p indel_set1/raw_results/4f_NR_hdp_gamma_beta_1
+nice Rscript indel_set1/code/4f_NR_hdp_gamma_beta_1.R indel/raw_results/4f_NR_hdp_gamma_beta_1 1076753 >& indel/raw_results/4f_NR_hdp_gamma_beta_1/1076753.log
