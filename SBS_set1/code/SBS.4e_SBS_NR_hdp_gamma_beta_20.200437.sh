b9253bbde2b1376db5b89bbda78c1f7365567698
Steven G. Rozen <steve@steverozen.net>
2022-10-10 09:47:26 +0800

    more cleanup

--- SBS_set1/code/SBS.4e_SBS_NR_hdp_gamma_beta_20.200437.sh
+++
@@ -1,9 +0,0 @@
-#PBS -q super
-#PBS -l nodes=1:ppn=20
-#PBS -N 4e_SBS_NR_hdp_gamma_beta_20.200437
-#PBS -o SBS/code/SBS.4e_SBS_NR_hdp_gamma_beta_20.200437.sh.out
-#PBS -e SBS/code/SBS.4e_SBS_NR_hdp_gamma_beta_20.200437.sh.err
-#PBS -S /bin/bash
-cd $PBS_O_WORKDIR
-mkdir -p SBS/raw_results/4e_SBS_NR_hdp_gamma_beta_20
-nice Rscript SBS/code/4e_SBS_NR_hdp_gamma_beta_20.R SBS/raw_results/4e_SBS_NR_hdp_gamma_beta_20 200437 >& SBS/raw_results/4e_SBS_NR_hdp_gamma_beta_20/200437.log
--- SBS_set1/code/SBS.4f_SBS_NR_hdp_gamma_beta_1.145879.sh
+++
@@ -1,9 +0,0 @@
-#PBS -q super
-#PBS -l nodes=1:ppn=20
-#PBS -N 4f_SBS_NR_hdp_gamma_beta_1.145879
-#PBS -o SBS/code/SBS.4f_SBS_NR_hdp_gamma_beta_1.145879.sh.out
-#PBS -e SBS/code/SBS.4f_SBS_NR_hdp_gamma_beta_1.145879.sh.err
-#PBS -S /bin/bash
-cd $PBS_O_WORKDIR
-mkdir -p SBS/raw_results/4f_SBS_NR_hdp_gamma_beta_1
-nice Rscript SBS/code/4f_SBS_NR_hdp_gamma_beta_1.R SBS/raw_results/4f_SBS_NR_hdp_gamma_beta_1 145879 >& SBS/raw_results/4f_SBS_NR_hdp_gamma_beta_1/145879.log
