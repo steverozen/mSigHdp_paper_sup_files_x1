d82eaa5bd8392419a16fb31a3ed36054020b37a0
Wu, Yang <wuyangff95@protonmail.com>
2022-10-10 14:48:25 +0800

    Removed all shell scripts for submitting to PBS queuing system

--- indel_set1/code/indel.4f_NR_hdp_gamma_beta_1.310111.sh
+++
@@ -1,9 +0,0 @@
-#PBS -q super
-#PBS -l nodes=1:ppn=20
-#PBS -N 4f_NR_hdp_gamma_beta_1.310111
-#PBS -o indel/code/indel.4f_NR_hdp_gamma_beta_1.310111.sh.out
-#PBS -e indel/code/indel.4f_NR_hdp_gamma_beta_1.310111.sh.err
-#PBS -S /bin/bash
-cd $PBS_O_WORKDIR
-mkdir -p indel/raw_results/4f_NR_hdp_gamma_beta_1
-nice Rscript indel/code/4f_NR_hdp_gamma_beta_1.R indel/raw_results/4f_NR_hdp_gamma_beta_1 310111 >& indel/raw_results/4f_NR_hdp_gamma_beta_1/310111.log
